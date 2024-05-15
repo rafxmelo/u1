08f437a60f9680405f5233516e5cba26951ee237
rafxmelo <mellotario@gmail.com>
2024-05-15 11:10:03 -0500

    first commit

---
+++ carl_sagan.rb
@@ -0,0 +1,52 @@
+# Rafael Melo
+carl  = {
+          :toast => 'cosmos',
+          :punctuation => [ ',', '.', '?' ],
+          :words => [ 'know', 'for', 'we']
+        }
+
+sagan = [
+          { :are => 'are', 'A' => 'a' },
+          { 'waaaaaay' => 'way', :th3 => 'the' },
+          'itself',
+          { 2 => ['to']}
+        ]
+
+puts "#{carl[:words][2].capitalize} #{sagan[1]['waaaaaay']} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{sagan[2]}#{carl[:punctuation][1]}"
+
+ghosts = [
+  { name: 'Inky', age: 4, loves: 'reindeers', net_worth: 25 },
+  { name: 'Pinky', age: 5, loves: 'garden tools', net_worth: 14 },
+  { name: 'Blinky', age: 7, loves: 'ninjas', net_worth: 18.03 },
+  { name: 'Clyde', age: 6, loves: 'yarn', net_worth: 0 }
+]
+
+ghosts.each do |ghost|
+  ghost_info  = "#{ghost[:name]} is #{ghost[:age]} years old, "
+  ghost_info += "loves #{ghost[:loves]} and "
+  ghost_info += "has #{ghost[:net_worth]} dollars in the bank."
+  puts ghost_info
+end
+
+require 'net/http'
+require 'json'
+
+url = 'https://dog.ceo/api/breeds/list/all'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+dog_breeds = JSON.parse(response)
+
+dog_breeds['message'].each do |breed, sub_breeds|
+  puts "* #{breed}"
+  sub_breeds.each do |sub_breed|
+    puts "  * #{sub_breed}"
+  end
+end
+
+url = 'https://data.winnipeg.ca/resource/d3jk-hb6j.json?$limit=306000'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+trees = JSON.parse(response)
+
+ash_trees = trees.select { |tree| tree['common_name']&.downcase&.include?('ash') }
+puts "Number of Ash Trees in Winnipeg: #{ash_trees.count}"
